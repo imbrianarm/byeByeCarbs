82775e8e1c7ae534a38c4c6d190f95874bf857d9
Brian <imbrianarm@gmail.com>
2019-03-03 10:15:28 -0500

    Updates

---
+++ precache-manifest.29725c27a1297a909cc002a5924ce46f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bc7ad8e9ac63189990a591cd359500f",
+    "url": "/byeByeCarbs/static/media/background.4bc7ad8e.jpg"
+  },
+  {
+    "revision": "ee3b9e2aca587b6c69de",
+    "url": "/byeByeCarbs/static/js/runtime~main.ee3b9e2a.js"
+  },
+  {
+    "revision": "7c893e1fc499dbff5761",
+    "url": "/byeByeCarbs/static/js/main.7c893e1f.chunk.js"
+  },
+  {
+    "revision": "991086699ed136b9d3af",
+    "url": "/byeByeCarbs/static/js/2.99108669.chunk.js"
+  },
+  {
+    "revision": "7c893e1fc499dbff5761",
+    "url": "/byeByeCarbs/static/css/main.77192509.chunk.css"
+  },
+  {
+    "revision": "9d9a06a430dce0d34641665f797075b1",
+    "url": "/byeByeCarbs/index.html"
+  }
+];
